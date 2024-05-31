df339a0d8b814042698165dae5a3f9917e1b61b1
Branko Zachemsky <bzachemsky@gmail.com>
2024-05-31 07:59:55 +0200

    WIP update to 5.3.3

---
+++ axopen.template.simple/axpansion/server/Controllers/ThemeDarkController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace axosimple.server.Controllers;
+
+[Route("/[controller]")]
+[ApiController]
+public class ThemeDarkController : ControllerBase
+{
+    public async Task<ActionResult> ChangeTheme([FromQuery] string theme)
+    {
+        Response.Cookies.Append("themeDark", theme);
+        return Redirect("/");
+    }
+}
