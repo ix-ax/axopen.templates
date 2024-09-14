ad01693024469a4421040ce115228f355eb63c4a
PTKu <61538034+PTKu@users.noreply.github.com>
2024-09-14 15:29:53 +0200

    due to templating #if #endif removed in Entry.cs replaced by selectable connector

--- axopen.template.simple/axpansion/twin/Entry.cs
+++ axopen.template.simple/axpansion/twin/Entry.cs
@@ -1,11 +1,10 @@
-﻿// ixsharpblazor
+﻿#define TIA
+// ixsharpblazor
 // Copyright (c) 2023 Peter Kurhajec (PTKu), MTS,  and Contributors. All Rights Reserved.
 // Contributors: https://github.com/ix-ax/axsharp/graphs/contributors
 // See the LICENSE file in the repository root for more information.
 // https://github.com/ix-ax/axsharp/blob/dev/LICENSE
 // Third party licenses: https://github.com/ix-ax/axsharp/blob/master/notices.md
-
-#define TIA
 
 using AXSharp.Connector;
 using System;
@@ -20,67 +19,58 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace axosimple
 {
-    public static class Entry
+    public class TwinConnectorSelector
     {
-
-#if HWC
-        public static string TargetIp { get; } = Environment.GetEnvironmentVariable("AXTARGET"); // <- replace by your IP 
-        private static string Pass => Environment.GetEnvironmentVariable("AX_TARGET_PWD");       //Environment.GetEnvironmentVariable("AX_TARGET_PWD"); // <- Pass in the password that you have set up for the user. NOT AS PLAIN TEXT! Use user secrets instead.
-        private static string UserName = Environment.GetEnvironmentVariable("AX_USERNAME"); //<- replace by user name you have set up in your WebAPI settings        
-        private const bool IgnoreSslErrors = true; // <- When you have your certificates in order set this to false.
-
-        static string GetCertPath()
-        {
-            var fp = new FileInfo(Path.Combine(Assembly.GetExecutingAssembly().Location));
-            return Path.Combine(fp.DirectoryName, ".certs\\plc_line.cer");
-        }
-
-        static X509Certificate2 customCertificate = new X509Certificate2(GetCertPath());
-
-        private static bool CertificateValidation(HttpRequestMessage requestMessage, X509Certificate2 certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-        {
-            return certificate.Thumbprint == customCertificate.Thumbprint;
-        }
-
-        public static axosimpleTwinController Plc { get; }
-            = new(ConnectorAdapterBuilder.Build()
-                .CreateWebApi(TargetIp, UserName, Pass, CertificateValidation, IgnoreSslErrors));
-#endif
-
-#if TIA
         public static string TargetIp { get; } = Environment.GetEnvironmentVariable("AXTARGET"); // <- replace by your IP 
         private static string Pass => @"123ABCDabcd$#!"; //Environment.GetEnvironmentVariable("AX_TARGET_PWD");       //Environment.GetEnvironmentVariable("AX_TARGET_PWD"); // <- Pass in the password that you have set up for the user. NOT AS PLAIN TEXT! Use user secrets instead.
         private static string UserName = "adm"; //Environment.GetEnvironmentVariable("AX_USERNAME"); //<- replace by user name you have set up in your WebAPI settings        
         private const bool IgnoreSslErrors = true; // <- When you have your certificates in order set this to false.
 
-        static string GetCertPath()
+        static string GetCertPathTia()
         {
             var fp = new FileInfo(Path.Combine(Assembly.GetExecutingAssembly().Location));
             return Path.Combine(fp.DirectoryName, ".certs\\TIA\\Communication.cer");
         }
 
-        static X509Certificate2 customCertificate = new X509Certificate2(GetCertPath());
+        static readonly X509Certificate2 TiaCertificate = new X509Certificate2(GetCertPathTia());
 
-        private static bool CertificateValidation(HttpRequestMessage requestMessage, X509Certificate2 certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        private static bool CertificateValidationTia(HttpRequestMessage requestMessage, X509Certificate2 certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
         {
-            return certificate.Thumbprint == customCertificate.Thumbprint;
+            return certificate.Thumbprint == TiaCertificate.Thumbprint;
         }
 
-        public static axosimpleTwinController Plc { get; }
-            = new(ConnectorAdapterBuilder.Build()
-                .CreateWebApi(TargetIp, UserName, Pass, CertificateValidation, IgnoreSslErrors));
-#endif
+        static string GetCertPathHwc()
+        {
+            var fp = new FileInfo(Path.Combine(Assembly.GetExecutingAssembly().Location));
+            return Path.Combine(fp.DirectoryName, ".certs\\plc_line.cer");
+        }
 
-#if TIA_NON_SECURE
-        public static string TargetIp { get; } = "192.168.0.10";//Environment.GetEnvironmentVariable("AXTARGET"); // <- replace by your IP 
-        private static string Pass => string.Empty; //Environment.GetEnvironmentVariable("AX_TARGET_PWD");       //Environment.GetEnvironmentVariable("AX_TARGET_PWD"); // <- Pass in the password that you have set up for the user. NOT AS PLAIN TEXT! Use user secrets instead.
-        private static string UserName = "Everybody"; //Environment.GetEnvironmentVariable("AX_USERNAME"); //<- replace by user name you have set up in your WebAPI settings        
-        private const bool IgnoreSslErrors = true; // <- When you have your certificates in order set this to false.
+        static readonly X509Certificate2 HwcCertificate = new X509Certificate2(GetCertPathHwc());
 
-        public static axosimpleTwinController Plc { get; }
+        private static bool CertificateValidationHwc(HttpRequestMessage requestMessage, 
+                                                    X509Certificate2 certificate, 
+                                                    X509Chain chain, 
+                                                    SslPolicyErrors sslPolicyErrors)
+        {
+            return certificate.Thumbprint == HwcCertificate.Thumbprint;
+        }
+        
+        public static axosimpleTwinController TiaPlc { get; }
             = new(ConnectorAdapterBuilder.Build()
-                .CreateWebApi(TargetIp, UserName, Pass, IgnoreSslErrors));
-#endif        
+                .CreateWebApi(TargetIp, UserName, Pass, CertificateValidationTia, IgnoreSslErrors));
+     
+        
+        public static axosimpleTwinController HwcPlc { get; }
+            = new(ConnectorAdapterBuilder.Build()
+                .CreateWebApi(TargetIp, UserName, Pass, CertificateValidationHwc, IgnoreSslErrors));
+        
+        public static axosimpleTwinController TiaNonSecurePlc { get; }
+            = new(ConnectorAdapterBuilder.Build()
+                .CreateWebApi(TargetIp, "Everybody",string.Empty, IgnoreSslErrors));
     }
-
+    
+    public static class Entry
+    {
+        public static axosimpleTwinController Plc { get; } = TwinConnectorSelector.TiaPlc;
+    }
 }
